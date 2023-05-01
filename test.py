1eacc4cf52a3aa4636820b713fc638c74fef73f7
pastullo <flacchio@gmail.com>
2023-05-01 19:25:21 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+"""Python file to serve as the frontend"""
+import streamlit as st
+from streamlit_chat import message
+
+# from langchain.chains import ConversationChain
+# from langchain.llms import OpenAI
+
+from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain.prompts import PromptTemplate
+
+
+loader = UnstructuredPDFLoader("book2.pdf")
+data = loader.load()
+
+print (f'You have {len(data)} document(s) in your data')
+print (f'There are {len(data[0].page_content)} characters in your document')
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(data)
