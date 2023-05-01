2ca8246b184d80d2206ed3cec30aab7ba2302675
pastullo <flacchio@gmail.com>
2023-05-01 19:41:16 +0200

    Update test.py

--- test.py
+++ test.py
@@ -10,12 +10,14 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document
 from langchain.prompts import PromptTemplate
 
-
+st.write('Loading book')
 loader = UnstructuredPDFLoader("book2.pdf")
 data = loader.load()
 
-print (f'You have {len(data)} document(s) in your data')
+# print (f'You have {len(data)} document(s) in your data')
+st.write('You have {len(data)} document(s) in your data')
 print (f'There are {len(data[0].page_content)} characters in your document')
+st.write(f'There are {len(data[0].page_content)} characters in your document')
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 texts = text_splitter.split_documents(data)
